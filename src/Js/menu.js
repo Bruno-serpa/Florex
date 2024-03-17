0f3774f23eb6a289439b889091a6eb3cfb94c7c2
Bruno Serpa <137077566+Bruno-serpa@users.noreply.github.com>
2024-03-17 16:27:13 -0300

    add header e reponsividade

--- src/Js/menu.js
+++ src/Js/menu.js
@@ -1,6 +1,12 @@
 const hamburger = document.querySelector(".hamburger");
 const nav = document.querySelector(".containerNav");
+const remover = document.querySelector(".containerHeader");
+const main = document.querySelector("containerMain");
+const footer = document.querySelector("containerFooter");
 
-hamburger.addEventListener("click", () =>
-    nav.classList.toggle("active")
-);
+hamburger.addEventListener("click", () => {
+    nav.classList.toggle("active");
+    remover.style.display = nav.classList.contains("active") ? "none" : "block";
+    main.style.display = nav.classList.contains("active") ? "none" : "block";
+    footer.style.display = nav.classList.contains("active") ? "none" : "block";
+});
